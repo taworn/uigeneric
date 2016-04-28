5954c84d43ae3445b472d5f29e23fc4181f97836
Taworn T <taworn@gmail.com>
2016-04-28 13:00:30 +0700

    - Changed AndroidManifest to start at MainActivity.

--- app/src/main/java/diy/uigeneric/MainActivity.java
+++ app/src/main/java/diy/uigeneric/MainActivity.java
@@ -1,7 +1,12 @@
 package diy.uigeneric;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import diy.uigeneric.sample.SampleListActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void onSampleClick(View view) {
+        startActivity(new Intent(this, SampleListActivity.class));
+    }
+
+    public void onSampleServerClick(View view) {
+    }
+
+    public void onSettingsClick(View view) {
+    }
+
+    public void onAboutClick(View view) {
     }
 
 }
